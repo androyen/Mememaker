8760bf6afa4013f3439b1afc80560295525d68f1
Androyen <androyen@aol.com>
2014-10-24 23:09:15 -0500

    Writing to internal storage

--- app/src/main/java/com/teamtreehouse/mememaker/utils/FileUtilities.java
+++ app/src/main/java/com/teamtreehouse/mememaker/utils/FileUtilities.java
@@ -36,6 +36,7 @@ public class FileUtilities {
         try {
             InputStream in = assetManager.open(assetName);
             FileOutputStream out = new FileOutputStream(fileToWrite);
+            copyFile(in, out);
 
             //Close input and output streams
             in.close();
@@ -45,6 +46,22 @@ public class FileUtilities {
         }
 
 
+    }
+
+    //Reusable copy file method
+    private static void copyFile(InputStream in, OutputStream out) throws IOException {
+
+        //Read from the input stream to write to the outputstream
+
+        //Create 1 byte buffer
+        byte[] buffer = new byte[1024];
+        int read;
+
+        //While there is data of one byte in the inputStream
+        while((read = in.read(buffer)) != -1) {
+            //Write to the outputStream the buffer and the data
+            out.write(buffer, 0, read);
+        }
     }
 
     public static Uri saveImageForSharing(Context context, Bitmap bitmap,  String assetName) {
